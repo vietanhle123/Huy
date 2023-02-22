e9087a21bade1eaa17670cd735edcfab0c18e38a
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-02-22 15:32:40 +0700

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -13,5 +13,5 @@ module.exports = async function (context, req) {
 }
 
 module.exports = async function (context, req){
-    context.res.json({text: "Hello from the API"});
+    context.res.json({text: "Hello from the API (by HuyDang)"});
 };
